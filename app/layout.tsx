be9986478edc842b6102863989ce1342340043dc
User <user@example.com>
2025-08-05 12:20:17 +0530

    Install Google Tag Manager (GTM-M85GNZTS)
    - Replace GA4 direct implementation with GTM
    - Add GTM script to head section for optimal loading
    - Add noscript fallback iframe after body tag
    - Enable centralized tag management through GTM dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,21 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-GHPG80940G"
-        />
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-GHPG80940G');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M85GNZTS');`,
           }}
         />
+        {/* End Google Tag Manager */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
@@ -41,6 +38,16 @@ html {
         `}</style>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M85GNZTS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
